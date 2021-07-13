bc9c84e93d838eda458ef8e8d51ffd1684e99cd9
Chen Xu <windoze@0d0a.com>
2021-07-14 00:20:59 +0800

    API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod singleton_cache;
+mod confidential_client;
+
+pub use confidential_client::{ConfidentialClientApplicationError, AccessToken, ClientCredential, ConfidentialClientApplicationBuilder, ConfidentialClientApplication};
